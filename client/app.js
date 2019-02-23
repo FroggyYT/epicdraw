0743c80291e3609e4ab95adbd84acd64286eb9da
Luke <lukebbarrier@gmail.com>
2019-02-23 17:03:14 -0500

    init

--- client/app.js
+++ client/app.js
@@ -18,7 +18,7 @@ function setup() {
     stroke(0);
     sockets = d;
     for (socket of d) {
-      image(ak, socket["pos"][0] - 25/2, socket["pos"][1] - 25/2, 25, 25);
+      image(ak, socket["pos"][0] - 50, socket["pos"][1] - 50, 100, 100);
     }
   });
 }
