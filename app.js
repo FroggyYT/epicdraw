0aaf73ee17e15cee847610abf1320a8bd91185b9
FroggyYT <44011185+FroggyYT@users.noreply.github.com>
2019-12-11 12:30:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,61 +12,10 @@ var io = require("socket.io")(server,{});
 
 var SOCKETS = [];
 
-var TSERIES = [];
-
-setInterval(() => {
-  if (SOCKETS.length > 0) {
-    var zomX = Math.random() * 500 + 50;
-    var zomY = 700;
-    TSERIES.push({x:zomX, y:zomY, health:100});
-  } else {
-    TSERIES = [];
-  }
-}, 5000);
-
-setInterval(() => {
-  for (var i in TSERIES) {
-    TSERIES[i].y -= 10;
-  }
-  io.emit("tseries", [TSERIES,SOCKETS]);
-}, 500);
-
 io.on("connection", (s) => {
-  SOCKETS.push({id:s.id, pos:[300, 300]});
+  SOCKETS.push({id:s.id, points:[]});
   var socketIndex;
-
-  s.on("move", (d) => {
-    for (var i in SOCKETS) {
-      if (SOCKETS[i]["id"] == s.id) {
-        socketIndex = i;
-      }
-    }
-    if (d == "w") {
-      SOCKETS[socketIndex]["pos"][1] += -0.5;
-    }
-    if (d == "a") {
-      SOCKETS[socketIndex]["pos"][0] += -0.5;
-    }
-    if (d == "s") {
-      SOCKETS[socketIndex]["pos"][1] += 0.5;
-    }
-    if (d == "d") {
-      SOCKETS[socketIndex]["pos"][0] += 0.5;
-    }
-    io.emit("update", [TSERIES,SOCKETS]);
-  });
-
-  s.on("attack", (d) => {
-    for (var i in TSERIES) {
-      if (d.x < TSERIES[i].x + 50 && d.x > TSERIES[i].x - 50 && d.y < TSERIES[i].y + 50 && d.y > TSERIES[i].y - 50) {
-        TSERIES[i].health -= 5;
-        if (TSERIES[i].health == 0) {
-          TSERIES.splice(i, 1);
-        }
-      }
-    }
-  });
-
+  
   s.on("disconnect", () => {
     for (var i in SOCKETS) {
       if (SOCKETS[i]["id"] == s.id) {
@@ -74,6 +23,5 @@ io.on("connection", (s) => {
       }
     }
     SOCKETS.splice(socketIndex, 1);
-    io.emit("update", [TSERIES,SOCKETS]);
   });
 });
