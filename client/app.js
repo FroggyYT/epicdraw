d14094a21fd4cdfc6562c4584cf65e839c066bd2
FroggyYT <44011185+FroggyYT@users.noreply.github.com>
2019-12-11 12:37:53 -0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,87 +1,29 @@
 var s;
 
-var pew;
-var tseries;
-
 function preload() {
   s = io();
-  pew = loadImage("/client/pew.png");
-  tseries = loadImage("/client/tseries.png");
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 600);
   background(51);
-  s.on("update", (d) => {
-    background(51);
-    for (socket of d[1]) {
-      image(pew, socket["pos"][0] - 50, socket["pos"][1] - 50, 100, 100);
-    }
-    for (tserie of d[0]) {
-      image(tseries, tserie.x - 50, tserie.y - 50, 100, 100);
-    }
-  });
-  s.on("tseries", (d) => {
-    background(51);
-    for (socket of d[1]) {
-      image(pew, socket["pos"][0] - 50, socket["pos"][1] - 50, 100, 100);
-    }
-    for (tserie of d[0]) {
-      image(tseries, tserie.x - 50, tserie.y - 50, 100, 100);
-    }
-  });
 }
 
-var movingW = false;
-var movingA = false;
-var movingS = false;
-var movingD = false;
+s.on("update", d => {
+  background(51);
+  for (var i of d) {
+    for (var j of d.points) {
+      fill(255);
+      noStroke();
+      ellipse(j.x, j.y, 5, 5); 
+    }
+  }
+});
 
-setInterval(() => {
-  if (movingW) {
-    s.emit("move", "w");
-  }
-  if (movingA) {
-    s.emit("move", "a");
-  }
-  if (movingS) {
-    s.emit("move", "s");
-  }
-  if (movingD) {
-    s.emit("move", "d");
-  }
-}, 1);
-
-function keyPressed() {
-  if (key == "w" || key == "W") {
-    movingW = true;
-  }
-  if (key == "a" || key == "A") {
-    movingA = true;
-  }
-  if (key == "s" || key == "S") {
-    movingS = true;
-  }
-  if (key == "d" || key == "D") {
-    movingD = true;
-  }
-}
-
-function keyReleased() {
-  if (key == "w" || key == "W") {
-    movingW = false;
-  }
-  if (key == "a" || key == "A") {
-    movingA = false;
-  }
-  if (key == "s" || key == "S") {
-    movingS = false;
-  }
-  if (key == "d" || key == "D") {
-    movingD = false;
-  }
+function mouseDragged() {
+  s.emit("draw", {x:mouseX, y:mouseY});
 }
 
 function mousePressed() {
-  s.emit("attack", {x:mouseX, y:mouseY});
+  s.emit("draw", {x:mouseX, y:mouseY}); 
 }
